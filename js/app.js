b0ca82f045e0d3a5157790e8488dcaaa733d6e11
George-Fran <george.gutierrez@tecsup.edu.pe>
2024-06-28 19:07:14 -0500

    ppt game

---
+++ js/app.js
@@ -0,0 +1,72 @@
+let opciones = document.querySelectorAll('div.opcion');
+const modal = document.querySelector(".modal");
+const modal_resultado = document.querySelector(".modal__resultado");
+const modal_imagen = document.querySelector(".modal__imagen");
+const modal_texto = document.querySelector(".modal__texto");
+const cerrar = document.querySelector(".boton");
+opciones.forEach((opcion, id) => {
+    opcion.addEventListener('click', () => {
+        const opcionComputadora = Math.floor(Math.random() * 3);
+        console.log("Opcion computador:", opcionComputadora);
+        console.log("Escogiste opcion: ", id);
+        let eleccionJugador = "";
+        let eleccionBot = "";
+        switch (id) {
+            case 0:
+                eleccionJugador = "piedra";
+                break;
+            case 1:
+                eleccionJugador = "papel";
+                break;
+            case 2:
+                eleccionJugador = "tijera"
+                break;
+            default:
+                eleccionJugador = "elpepe";
+                break;
+        }
+        switch (opcionComputadora) {
+            case 0:
+                eleccionBot = "piedra";
+                break;
+            case 1:
+                eleccionBot = "papel";
+                break;
+            case 2:
+                eleccionBot = "tijera"
+                break;
+            default:
+                eleccionBot = "elpepe";
+                break;
+        }
+        if (id === opcionComputadora) {
+            console.log("Empate");
+            modal_resultado.innerHTML = "Vaya, parece que tú y la computadora han quedado en empate.";
+            modal_imagen.src = "img/bot.svg";
+            modal_texto.innerHTML = `Elegiste ${eleccionJugador}, y parece que la computadora te imitó. Por eso, este juego termina en empate. ¡Inténtalo de nuevo!`;
+        } else if ((id - opcionComputadora) == 1 || (id - opcionComputadora) == -2) {
+            modal_imagen.src = "img/winner.svg";
+            modal_resultado.innerHTML = "¡Felicidades! Has ganado, la computadora ha sido derrotada.";
+            modal_texto.innerHTML = `Elegiste ${eleccionJugador} y la computadora escogió ${eleccionBot}. ¡Y asi dicen que la IA remplazara al humano!`
+        } else {
+            modal_imagen.src = "img/lose.svg";
+            modal_resultado.innerHTML = "¡Perdiste! La computadora triunfo esta vez.";
+            modal_texto.innerHTML = `Elegiste ${eleccionJugador} y la computadora escogió ${eleccionBot}. ¡No esperes más, vuelve a intentarlo antes de que la IA domine al mundo!`
+        }
+
+        if (modal.classList.contains("modal--close")) {
+            modal.classList.remove("modal--display");
+            setTimeout(() => {
+                modal.classList.remove("modal--close");
+            }, 10);
+        }
+    });
+});
+cerrar.addEventListener("click", () => {
+    modal.classList.add("modal--close");
+});
+modal.addEventListener('transitionend', () => {
+    if (modal.classList.contains('modal--close')) {
+        modal.classList.add('modal--display');
+    }
+});
---
+++ readme.md
@@ -0,0 +1,15 @@
+# Piedra, Papel o Tijeras
+
+<p align="center">
+  <img src="ruta/a/tu/imagen.png" alt="Piedra, Papel o Tijeras">
+</p>
+
+## Descripción
+
+Bienvenido al proyecto "Piedra, Papel o Tijeras". Este es un juego clásico en el que dos jugadores eligen una de las tres opciones: piedra, papel o tijeras. Las reglas del juego son simples:
+
+- **Piedra** vence a tijeras.
+- **Tijeras** vence a papel.
+- **Papel** vence a piedra.
+
+¡Diviértete jugando y mejora tus habilidades estratégicas!
